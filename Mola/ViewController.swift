d10404c20156f82512eb07fb2647951c60bef1b6
onurbosstan <onurbosstan@gmail.com>
2024-10-02 14:21:06 +0300

    Initial Commit

---
+++ Mola/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mola
+//
+//  Created by Onur Bostan on 2.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

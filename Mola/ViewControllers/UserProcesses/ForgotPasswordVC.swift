91016a569a212f5302292af222e61c634d342a7b
onurbosstan <onurbosstan@gmail.com>
2024-10-07 20:52:22 +0300

    Coding has been completed in ForgotPasswordVC. When the user forgets his password, an e-mail will be sent to his e-mail address after entering his e-mail address.

--- Mola/ViewControllers/UserProcesses/ForgotPasswordVC.swift
+++ Mola/ViewControllers/UserProcesses/ForgotPasswordVC.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ForgotPasswordVC: UIViewController {
     @IBOutlet weak var emailText: UITextField!
+    let viewModel = ForgotPasswordViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +17,20 @@ class ForgotPasswordVC: UIViewController {
     }
     
     @IBAction func resetButton(_ sender: Any) {
-        
+        if let email = self.emailText.text {
+            self.viewModel.forgotPassword(email: email) { error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: "You entered the wrong e-mail adress")
+                } else {
+                    self.makeAlert(titleInput: "Success!", messageInput: "A link has been sent to your e-mail address to reset your password")
+                }
+            }
+        }
     }
-
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
 }

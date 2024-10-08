209f240dabace7e1f994dd3a1ea891169f25f141
onurbosstan <onurbosstan@gmail.com>
2024-10-08 21:32:56 +0300

    RegisterVC coding was completed and the user was registered, and Security was created in passwordTexts.

--- Mola/ViewControllers/UserProcesses/RegisterVC.swift
+++ Mola/ViewControllers/UserProcesses/RegisterVC.swift
@@ -10,17 +10,36 @@ import UIKit
 class RegisterVC: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    var viewModel = RegisterViewModel()
+    var iconClick = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func closeKeyboard() {
+        view.endEditing(true)
     }
     @IBAction func registerButton(_ sender: Any) {
-        
+        if let email = self.emailText.text, let password = self.passwordText.text {
+            self.viewModel.signUp(email: email, password: password) { _ in
+                self.makeAlert(titleInput: "Success!", messageInput: "You have successfully registered.")
+            }
+        }
     }
-    
     @IBAction func hidePasswordButton(_ sender: Any) {
-        
+        if iconClick {
+            passwordText.isSecureTextEntry = false
+        } else {
+            passwordText.isSecureTextEntry = true
+        }
+        iconClick = !iconClick
+    }
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
 }

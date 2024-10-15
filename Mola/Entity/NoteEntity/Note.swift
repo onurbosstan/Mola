99946dff4c455414431fc5d6e4fa53ffe13c23fe
onurbosstan <onurbosstan@gmail.com>
2024-10-15 21:28:52 +0300

    HomeVC is complete, users will be able to save any notes they want in "Mola"

---
+++ Mola/Entity/NoteEntity/Note.swift
@@ -0,0 +1,13 @@
+//
+//  Note.swift
+//  Mola
+//
+//  Created by Onur Bostan on 15.10.2024.
+//
+
+import Foundation
+
+struct Note {
+    let content: String
+    let date: Date
+}
--- Mola/ViewControllers/TabBar'sViewControllers/Home/HomeVC.swift
+++ Mola/ViewControllers/TabBar'sViewControllers/Home/HomeVC.swift
@@ -6,18 +6,50 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
 
 class HomeVC: UIViewController {
-
+    @IBOutlet weak var textView: UITextView!
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        textView.delegate = self
     }
+    
     
     @IBAction func saveButton(_ sender: Any) {
-        
-        
+        guard let noteText = textView.text, !noteText.isEmpty else {
+            return
+        }
+        guard let user = Auth.auth().currentUser else {
+            print("User not authenticated")
+            return
+        }
+        let noteData: [String: Any] = [
+            "content": noteText,
+            "date": Timestamp(date: Date()),
+            "userId": user.uid
+        ]
+        db.collection("Notes").addDocument(data: noteData) { error in
+            if let error = error {
+                self.makeAlert(titleInput: "Hata!", messageInput: "Notunuz kaydedilmedi.")
+            } else {
+                self.textView.text = ""
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
-    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+}
+extension HomeVC: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+    }
 }

c9a5dcfa8bc2977e3daf4eee1468037fb9d87b43
onurbosstan <onurbosstan@gmail.com>
2024-10-20 01:37:53 +0300

    MemoriesVC and MemoriesUploadVC the pages were designed

---
+++ Mola/Cells/Memories/MemoriesCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoriesCell.swift
+//  Mola
+//
+//  Created by Onur Bostan on 20.10.2024.
+//
+
+import UIKit
+
+class MemoriesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Mola/ViewControllers/TabBar'sViewControllers/Memories/MemoriesUploadVC.swift
@@ -0,0 +1,18 @@
+//
+//  MemoriesDetailVC.swift
+//  Mola
+//
+//  Created by Onur Bostan on 20.10.2024.
+//
+
+import UIKit
+
+class MemoriesUploadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}

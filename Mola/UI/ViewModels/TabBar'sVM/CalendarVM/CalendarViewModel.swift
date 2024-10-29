5f3574bcc201d961e76ef94878764bcfb1a28b95
onurbosstan <onurbosstan@gmail.com>
2024-10-29 16:46:27 +0300

    CalendarVM created

--- Mola/UI/ViewModels/TabBar'sVM/CalendarVM/CalendarViewModel.swift
+++ Mola/UI/ViewModels/TabBar'sVM/CalendarVM/CalendarViewModel.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 class CalendarViewModel {
+    private var notes: [Date: [String]] = [:]
     
+    func getNotes(for date: Date) -> [String] {
+        return notes[date] ?? []
+    }
     
+    func addNote(_ note: String, for date: Date) {
+        if notes[date] != nil {
+            notes[date]?.append(note)
+        } else {
+            notes[date] = [note]
+        }
+    }
 }
